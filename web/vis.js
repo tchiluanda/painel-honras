5f787a3539d114d39b61020b8d6978eb9312756f
tiagombp <tiagombp@gmail.com>
2020-05-05 09:57:02 -0300

    começa a dar forma para a visualização. dessa vez vou tentar estruturar mais ou menos como (acho que) o Russell faz.

--- web/vis.js
+++ web/vis.js
@@ -10,3 +10,35 @@ const altura_container_vis = altura_janela - altura_logo - altura_header - altur
 // atenção! a margen não entra nesse cálculo, então é importante zerar as margens verticais entre os elementos sendo medidos.
 console.log(altura_container_vis);
 d3.select(".vis-container").style("height", altura_container_vis + "px");
+
+
+const svg_prin = d3.select("svg.vis-principal");
+
+const margins = {
+    top : 10,
+    right : 10,
+    bottom: 10,
+    left: 10
+}
+
+function desenha_principal() {
+    const h = svg_prin.style("height");
+    const w = svg_prin.style("width");
+
+    // scales
+
+    
+
+
+}
+
+function init() {
+
+}
+
+d3.csv("dados/dados.csv").then(function(dados) {
+    console.log(dados.columns);
+    // ["", "data", "tipo_divida", "Credor", "contrato", "tipo_credor", "mutuario", "tipo_mutuario", "Status", "valor", "mes", "ano", "mes_ano", "data_mes", "pos_ini_mutuario", "pos_ini_Credor", "pos_ini_tipo_divida", "pos_ini_ano"]
+
+    console.log(group_by_sum(dados, "mutuario", "valor", true));
+})
