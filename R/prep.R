4a2e7feedcfaf8f43e8973d623894e937c26264d
tiagombp <tiagombp@gmail.com>
2020-12-07 00:01:52 -0300

    atualização de novembro

--- R/prep.R
+++ R/prep.R
@@ -15,7 +15,7 @@ loadfonts()
 
 # importa dados -----------------------------------------------------------
 
-honras <- read.csv2("./R/relatorio_honras_2020_10.csv",
+honras <- read.csv2("./R/relatorio_honras_2020_11.csv",
                     skip = 10, stringsAsFactors = FALSE,
                     fileEncoding="latin1")
 
