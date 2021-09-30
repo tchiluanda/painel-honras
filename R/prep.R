505189b0809267480ad3d69bb8a34d882b861cb6
tiagombp <tiagombp@gmail.com>
2021-09-30 20:04:20 -0300

    atualização do script para dados de setembro/2021

--- R/prep.R
+++ R/prep.R
@@ -15,7 +15,7 @@ loadfonts()
 
 # importa dados -----------------------------------------------------------
 
-honras <- read.csv2("./R/relatorio_honras_2021_08.csv",
+honras <- read.csv2("./R/relatorio_honras_2021_09.csv",
                     skip = 10, stringsAsFactors = FALSE,
                     fileEncoding="latin1")
 
